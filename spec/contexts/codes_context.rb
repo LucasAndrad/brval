988631afbbe2908e6b2c97a2a455a0bf46ea75bb
lucasandrad <lucasandrad@yandex.com>
2018-12-31 11:08:25 -0200

    Add a test for invalid cpf

--- spec/contexts/codes_context.rb
+++ spec/contexts/codes_context.rb
@@ -1,4 +1,5 @@
 RSpec.shared_context "codes", a: :b do
   let!(:cpf_valid) { '98107788052' }
   let!(:cpf_valid_mask) { '084.701.700-15' }
+  let!(:cpf_invalid) { '98107788000' }
 end
--- spec/lib/brval_spec.rb
+++ spec/lib/brval_spec.rb
@@ -11,6 +11,11 @@ RSpec.describe Brval do
         valid = Brval.cpf_valid?(cpf_valid)
         expect(valid).to be(true)
       end
+
+      it "should return false #cpf_valid?" do
+        valid = Brval.cpf_valid?(cpf_invalid)
+        expect(valid).to be(false)
+      end
     end
 
   end
