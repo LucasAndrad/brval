0df378bac465aa998fa4900a3e22ff63a39f6959
lucasandrad <lucasandrad@yandex.com>
2018-12-31 00:00:14 -0200

    Fix some details, now Cnpj is working fine, and aff .gem files to gitignore

--- lib/brval.rb
+++ lib/brval.rb
@@ -1,10 +1,12 @@
 require "brval/version"
 require "brval/cpf_call"
+require "brval/cnpj_call"
 
 module Brval
   extend CpfCall
+  extend CnpjCall
   class Error < StandardError; end
   
-  
+
 
 end
--- lib/brval/cnpj.rb
+++ lib/brval/cnpj.rb
@@ -10,7 +10,7 @@ module Brval
     NULLS = %w{11111111111111 22222222222222 33333333333333 44444444444444 55555555555555 66666666666666 77777777777777 88888888888888 99999999999999 00000000000000}.freeze
 
     # function from https://gist.github.com/lucascaton/1109488
-    def check_cnpj cnpj=nil
+    def validate_cnpj cnpj=nil
       # remote_mask(cpf)
       # Add errors after initialize CNPJ this class if cpf has wrong value
       return false if cnpj.nil?
