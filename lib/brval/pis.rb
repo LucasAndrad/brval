b37d08ac941c42edc13a1df5a88d9066a4b3332b
lucasandrad <lucasandrad@yandex.com>
2019-01-01 19:31:22 -0200

    Create Pis class and the validation function

---
+++ lib/brval/pis.rb
@@ -0,0 +1,25 @@
+module Brval
+  class Pis
+
+    def valid? pis
+      validate_cpf(pis)
+    end
+
+    private
+
+    def validate_pis pis
+      return false if pis.nil?
+
+      value = pis.scan /[0-9]/
+      if value.length == 11
+        value = value.collect{|x| x.to_i}
+        sum = 3*value[0] + 2*value[1] + 9*value[2] + 8*value[3] + 7*value[4] + 6*value[5] + 5*value[6] + 4*value[7] + 3*value[8] + 2*value[9]
+        result = 11 - sum.modulo(11)
+        return true if result == value[10]
+        return true if value[10] == 0 && (result == 10 || result == 11)
+      end
+      return false
+    end
+
+  end
+end
