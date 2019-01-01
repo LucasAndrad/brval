33342d5ff583146dd62ff399d85819e59b265a73
lucasandrad <lucasandrad@yandex.com>
2019-01-01 19:12:42 -0200

    Add tests for cnpj valid and invalid values

--- spec/contexts/codes_context.rb
+++ spec/contexts/codes_context.rb
@@ -2,4 +2,9 @@ RSpec.shared_context "codes", a: :b do
   let!(:cpf_valid) { '98107788052' }
   let!(:cpf_valid_mask) { '084.701.700-15' }
   let!(:cpf_invalid) { '98107788000' }
+  
+  let!(:cnpj_valid) { '37711352000165' }
+  let!(:cnpj_valid_mask) { '23.428.153/0001-30' }
+  let!(:cnpj_invalid) { '37711352000100' }
+
 end
--- spec/lib/brval_spec.rb
+++ spec/lib/brval_spec.rb
@@ -7,15 +7,31 @@ RSpec.describe Brval do
   describe "Brval tests" do
 
     context "CPF values" do
-      it "should return true #cpf_valid?" do
+      it "should return true .cpf_valid?" do
         valid = Brval.cpf_valid?(cpf_valid)
         expect(valid).to be(true)
       end
 
-      it "should return false #cpf_valid?" do
+      it "should return false .cpf_valid?" do
         valid = Brval.cpf_valid?(cpf_invalid)
         expect(valid).to be(false)
       end
+
+      # Add a test with mask
+    end
+
+    context "CNPJ values" do
+      it "should return true .cnpj_valid?" do
+        valid = Brval.cnpj_valid?(cnpj_valid)
+        expect(valid).to be(true)
+      end
+
+      it "should return false .cnpj_valid?" do
+        valid = Brval.cnpj_valid?(cnpj_invalid)
+        expect(valid).to be(false)
+      end
+      
+      # Add a test with mask
     end
 
   end
