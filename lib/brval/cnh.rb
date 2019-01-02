6fd5049a239711754acb80a66ec4a01486680d14
lucasandrad <lucasandrad@yandex.com>
2019-01-02 16:25:27 -0200

    Refactor cnh functions variables, put all variables in english

--- lib/brval/cnh.rb
+++ lib/brval/cnh.rb
@@ -1,7 +1,14 @@
 module Brval
     # Validate CNH
   class Cnh
-    def self.valid?(cnh)
+
+    def valid? cnpj
+      validate_cnh(cnpj)
+    end
+
+    private
+
+    def validate_cnh(cnh)
       return false if cnh.length < 11
       return false if cnh[0] * 11 == cnh
 
@@ -9,32 +16,32 @@ module Brval
       cnh_digits = cnh[-2..-1]
       incr_dig2 = 0
 
-      soma = 0
+      sum = 0
       mult = 9
       (0..8).each do |i|
-        soma += cnh_without_digits[i].to_i * mult
+        sum += cnh_without_digits[i].to_i * mult
         mult -= 1
       end
 
-      digito1 = soma % 11
-      incr_dig2 = -2 if digito1 == 10
-      digito1 = 0 if digito1 > 9
+      digit1 = sum % 11
+      incr_dig2 = -2 if digit1 == 10
+      digit1 = 0 if digit1 > 9
 
-      soma = 0
+      sum = 0
       mult = 1
       (0..8).each do |i|
-        soma += (cnh_without_digits[i].to_i * mult)
+        sum += (cnh_without_digits[i].to_i * mult)
         mult += 1
       end
 
-      digito2 = if ((soma % 11) + incr_dig2) < 0
-                  11 + (soma % 11) + incr_dig2
+      digit2 = if ((sum % 11) + incr_dig2) < 0
+                  11 + (sum % 11) + incr_dig2
                 else
-                  (soma % 11) + incr_dig2
+                  (sum % 11) + incr_dig2
                 end
-      digito2 = 0 if digito2 > 9
+      digit2 = 0 if digit2 > 9
 
-      dig_to_compare = digito1.to_s + digito2.to_s
+      dig_to_compare = digit1.to_s + digit2.to_s
 
       return true if cnh_digits == dig_to_compare
       false
