21591759e748152b8e314a3f6d9147cc6013f1f1
lucasandrad <lucasandrad@yandex.com>
2019-01-02 00:46:34 -0200

    Fix some details of te validation methods

--- lib/brval/te.rb
+++ lib/brval/te.rb
@@ -13,6 +13,7 @@ module Brval
 
       value = te.scan /[0-9]/
       if value.length == 12
+        value = value.collect{|x| x.to_i}
         digit1 = calc_digit1(value)
         digit2 = calc_digit2(value, digit1)
         return true if value[10] == digit1 && value[11] == digit2
@@ -22,17 +23,17 @@ module Brval
     end
 
     def calc_digit1 nums
-      sum = value[0]*9 + value[1]*8 + value[2]*7 + value[3]*6 + value[4]*5 + value[5]*4 + value[6]*3 + value[7]*2
+      sum = nums[0]*9 + nums[1]*8 + nums[2]*7 + nums[3]*6 + nums[4]*5 + nums[5]*4 + nums[6]*3 + nums[7]*2
       check_result(nums, sum)
     end
 
     def calc_digit2 nums, digit1
-      sum = value[8]*4 + value[9]*3 + digit1*2
+      sum = nums[8]*4 + nums[9]*3 + digit1*2
       check_result(nums, sum)
     end
 
     def check_result nums, sum
-      rest = sum.module(11)
+      rest = sum.modulo(11)
       if rest == 0 || rest == 1
         if nums[8] == 0 && (nums[9] == 1 || nums[9] == 2)
           if rest == 0
