08d23c0c085f0e1915d22cf504c8f37a4799d5f0
lucasandrad <lucasandrad@yandex.com>
2019-01-12 19:07:40 -0200

    Created a generic class Val, to put all common code

---
+++ lib/brval/val.rb
@@ -0,0 +1,12 @@
+module Brval
+  class Val
+
+    attr_accessor :code
+
+    def initialize(code)
+      raise ArgumentError, 'The br code informed is nil' if code.nil?
+      @code = code.tr('^0-9', '')
+    end
+
+  end
+end
