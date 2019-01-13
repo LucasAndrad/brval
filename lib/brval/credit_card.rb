42f3853fe4c5a3631ae046f16765712a84ce70a5
lucasandrad <lucasandrad@yandex.com>
2019-01-13 00:07:19 -0200

    Fix credit card validation method

--- lib/brval/credit_card.rb
+++ lib/brval/credit_card.rb
@@ -4,22 +4,23 @@ module Brval
     def validate_code
       raise ArgumentError, 'Credit card number must have 16 digits' if @code.length != 16
       value = @code.scan /[0-9]/
-      if value.length == 11
-        sum = sum_nums(value)
+      if value.length == 16
+        sum = sum_nums(value[0..14])
         sum_complete = treat_sum(sum.digits.reverse)
-        return true if sum_complete - sum == value[15]
+        return true if sum_complete - sum == value[15].to_i
       end
       false
     end
 
     def sum_nums nums
       sum = 0
-      0..15.each do |i|
+      nums.each_with_index do |n, i|
+        num = n.to_i
         if i%2 != 0
-          sum += i 
-        else
-          num = (i*2).digits.sum
           sum += num
+        else
+          new_num = (num*2).digits.sum
+          sum += new_num
         end
       end
       sum
@@ -28,9 +29,9 @@ module Brval
     def treat_sum nums
       if nums.last != 0
         nums[0] = nums[0]+1
-        nums.last = 0
+        nums[nums.count-1] = 0
       end
-      nums
+      nums.join.to_i
     end
 
   end
