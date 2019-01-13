3553b4f4f22770043401e42dbffea418023b016b
lucasandrad <lucasandrad@yandex.com>
2019-01-13 13:15:20 -0200

    Fix renavam wrong names for variables

--- lib/brval/renavam.rb
+++ lib/brval/renavam.rb
@@ -3,18 +3,18 @@ module Brval
 
     private
 
-    def validate_code code
-      code = "00" + code if code.length == 9
-      ren_cut = code[0..9]
+    def validate_code
+      @code = "00" + @code if @code.length == 9
+      ren_cut = @code[0..9]
       ren_reverse = ren_cut.reverse[0..7]
       sum = 0
       (2..9).to_a.each do |i|
         sum += ren_reverse[i-2].to_i * i
       end
-      sum += code[0].to_i*3 + code[1].to_i*2
+      sum += @code[0].to_i*3 + @code[1].to_i*2
       result1 = 11 - (sum % 11)
       digit1 = (result1 == 10 || result1 == 11) ? 0 : result1
-      return true if code[10].to_i == digit1
+      return true if @code[10].to_i == digit1
       false
     end
 
