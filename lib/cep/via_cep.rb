c2c792ea8b8b9fad9f90c30f2f2e0a628c3ad382
lucasandrad <lucasandrad@yandex.com>
2019-01-17 00:34:24 -0200

    Start to add cep microservice call

---
+++ lib/cep/via_cep.rb
@@ -0,0 +1,34 @@
+class Cep::ViaCep
+
+  URL_HOME = 'https://viacep.com.br/ws/'.freeze
+  attr_accessor :cep, :url
+
+  def initialize cep
+    @cep = cep.tr('^0-9', '')
+    @url = URL_HOME + @cep + '/json'
+  end
+
+  def check
+    json = load_cep_json
+    json_valid?(json)
+  end
+
+  def info
+    
+  end
+
+  private
+
+  def load_cep_json
+    response = HTTParty.get(@url)
+    return nil if response.code != 200
+    JSON.parse(response.body)
+  end
+
+  def json_valid?(json)
+    byebug
+    return false if json['erro'].present?
+    json['logradouro'].present? && json['bairro'] && json['localidade'].present? && json['uf'].present?
+  end
+
+end
