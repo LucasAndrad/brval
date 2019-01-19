2d196ed188b07403944e14e7d94f1774319678f3
lucasandrad <lucasandrad@yandex.com>
2019-01-19 00:19:04 -0200

    Add new services to cep validation

--- lib/brval/cep_call.rb
+++ lib/brval/cep_call.rb
@@ -1,5 +1,6 @@
-require 'brval/cep/via_cep'
-require 'brval/cep/widenet'
+# require 'brval/cep/postmon'
+# require 'brval/cep/via_cep'
+# require 'brval/cep/widenet'
 
 module Brval::CepCall
 
@@ -11,8 +12,9 @@ module Brval::CepCall
 
   def call_apis(cep)
     via = Cep::ViaCep.new(cep).check
-    widenet = Cep::Widenet.new(cep).check if vie != true
-    return true if (via == true || widenet == true)
+    widenet = Cep::Widenet.new(cep).check if via != true
+    postmon = Cep::Postmon.new(cep).check if via != true && widenet != true
+    return true if (via == true || widenet == true || postmon == true)
     false
   end
 
