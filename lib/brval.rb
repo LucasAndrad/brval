7338d1f326a691031d637896742e42b2ca183eb5
lucasandrad <lucasandrad@yandex.com>
2019-01-19 00:19:28 -0200

    Add postmon to brval main class

--- lib/brval.rb
+++ lib/brval.rb
@@ -11,6 +11,7 @@ require 'brval/pis_call'
 require 'brval/renavam_call'
 require 'brval/te_call'
 
+require 'brval/cep/postmon'
 require 'brval/cep/via_cep'
 require 'brval/cep/widenet'
 require 'brval/cep_call'
