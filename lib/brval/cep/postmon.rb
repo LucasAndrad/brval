2641d8084a8ebcdf9ea87629ee8e77d86ac9fe8c
lucasandrad <lucasandrad@yandex.com>
2019-01-19 00:40:04 -0200

    Add a translate method to postman

--- lib/brval/cep/postmon.rb
+++ lib/brval/cep/postmon.rb
@@ -18,7 +18,7 @@ module Cep
 
     def info
       json = load_cep_json
-      # translate_params(json)
+      translate_params(json)
     end
 
     private
@@ -29,9 +29,24 @@ module Cep
       JSON.parse(response.body)
     end
 
-    def json_valid?(json)
+    def json_valid? json
       return false if json.nil?
       json.key?('bairro') && json.key?('logradouro') && json.key?('cidade') && json.key?('estado')
+    end
+
+    def translate_params json
+      json['cep'] = json['cep'].insert(5, '-')
+      json['address'] = json.delete 'logradouro'
+      json['complement'] = ''
+      json['neighborhood'] = json.delete 'bairro'
+      json['city'] = json.delete 'cidade'
+      json['state'] = json.delete 'estado'
+      json['unit'] = ''
+      json['ibge'] = json['cidade_info']['codigo_ibge']
+      json['gia'] = ''
+      json.delete 'estado_info'
+      json.delete 'cidade_info'
+      json
     end
 
   end
