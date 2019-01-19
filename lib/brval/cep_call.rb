01c8a34fe8121033a85054fee720a8f3c2a14e75
lucasandrad <lucasandrad@yandex.com>
2019-01-18 23:43:36 -0200

    Create a method to check if cep is valid at CepCall module

--- lib/brval/cep_call.rb
+++ lib/brval/cep_call.rb
@@ -1,13 +1,19 @@
-require "brval/cep/via_cep"
+require 'brval/cep/via_cep'
+require 'brval/cep/widenet'
 
 module Brval::CepCall
 
-  def cep_valid?
-    Cep::ViaCep.new('72110040').check
+  def cep_valid?(cep)
+    return false if cep.nil?
+    cep = cep.to_s.tr('^0-9', '')
+    call_apis(cep)
   end
-  
-  def nada
-    p 'nada nada'
+
+  def call_apis(cep)
+    via = Cep::ViaCep.new(cep).check
+    widenet = Cep::Widenet.new(cep).check if vie != true
+    return true if (via == true || widenet == true)
+    false
   end
 
 end
