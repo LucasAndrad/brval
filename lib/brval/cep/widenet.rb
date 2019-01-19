cfb4ea3ac741bcc6d45d88a52db5d2a7c3c87731
lucasandrad <lucasandrad@yandex.com>
2019-01-18 23:43:07 -0200

    Create API widenet service

---
+++ lib/brval/cep/widenet.rb
@@ -0,0 +1,39 @@
+module Cep
+  class Widenet
+
+    # Thanks you WideNet for this API
+    # apps.widenet.com.br/busca-cep/api-de-consulta
+
+    URL_HOME = 'http://apps.widenet.com.br/busca-cep/api/cep/'.freeze
+    attr_accessor :cep, :url
+
+    def initialize cep
+      @cep = cep.tr('^0-9', '')
+      @url = URL_HOME + @cep + '.json'
+    end
+
+    def check
+      json = load_cep_json
+      json_valid?(json)
+    end
+
+    def info
+      load_cep_json
+    end
+
+    private
+
+    def load_cep_json
+      response = HTTParty.get(@url)
+      return nil if response.code != 200
+      JSON.parse(response.body)
+    end
+
+    def json_valid?(json)
+      return false unless json['erro'].nil?
+      json.key?('district') && json.key?('city') && json.key?('address') && json.key?('state')
+    end
+
+    
+  end
+end
