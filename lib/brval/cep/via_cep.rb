ae613c5f9247dff63930d0d792b1ace9067a05b4
lucasandrad <lucasandrad@yandex.com>
2019-01-18 23:18:27 -0200

    Add a function to translate the json params

--- lib/brval/cep/via_cep.rb
+++ lib/brval/cep/via_cep.rb
@@ -15,7 +15,8 @@ module Cep
     end
 
     def info
-      
+      json = load_cep_json
+      translate_params(json)
     end
 
     private
@@ -29,6 +30,18 @@ module Cep
     def json_valid?(json)
       return false unless json['erro'].nil?
       json.key?('logradouro') && json.key?('bairro') && json.key?('localidade') && json.key?('uf')
+    end
+
+    def translate_params(json)
+      json['place'] = json.delete 'logradouro'
+      json['complement'] = json.delete 'complemento'
+      json['neighborhood'] = json.delete 'bairro'
+      json['locality'] = json.delete 'localidade'
+      json['state'] = json.delete 'uf'
+      json['unit'] = json.delete 'unidade'
+      json['ibge'] = json.delete 'ibge'
+      json['gia'] = json.delete 'gia'
+      json
     end
 
   end
